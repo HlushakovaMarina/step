f6fcf55610cde48d284b740855daef44cbb2f13c
HlushakovaMarina <truhina_m@mail.ru>
2025-02-20 20:58:34 +0300

    добавили классы

---
+++ src/L13_02_2025/L13_02_2025_ZOO2/L13_02_2025_ZOO2/OwnerFactory.java
@@ -0,0 +1,12 @@
+package L13_02_2025.L13_02_2025_ZOO2.L13_02_2025_ZOO2;
+
+import java.util.Random;
+
+public class OwnerFactory {
+
+    public static Owner next() {
+        return null;
+    }
+    Random random = new Random();
+    Owner owner = OwnerFactory.next();
+}
---
+++ src/L13_02_2025/L13_02_2025_ZOO2/L13_02_2025_ZOO2/PetFactory.java
@@ -0,0 +1,17 @@
+package L13_02_2025.L13_02_2025_ZOO2.L13_02_2025_ZOO2;
+
+import java.util.Random;
+
+public class PetFactory {
+
+    private String name;
+    Random random = new Random();
+
+
+
+    static Pet next(){
+
+    }
+
+    Pet pet = PetFactory.next();
+}
