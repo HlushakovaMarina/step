a8421e1bccc023bb0253e8db80bc1e3ddb0bd320
hlushakovamarina <truhina_m@mail.ru>
2025-02-27 13:26:55 +0300

    доработка

--- src/L24_02_2025/PaymentMethod/PayPal.java
+++ src/L24_02_2025/PaymentMethod/PayPal.java
@@ -3,6 +3,10 @@ package L24_02_2025.PaymentMethod;
 public class PayPal implements PaymentMethod {
     private String email;
 
+    public PayPal(String email) {
+        this.email=email;
+    }
+
     @Override
     public void pay(double amount) {
         System.out.println("Оплата " + amount + " через PayPal аккаунт " + email);
