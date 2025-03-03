0b075192a4470699e8e0bb55004c4c12bd7010b6
HlushakovaMarina <truhina_m@mail.ru>
2025-03-03 21:20:15 +0300

    Изучение коллеций

---
+++ src/L03_03_2025/students/FactoryStudents.java
@@ -0,0 +1,18 @@
+package L03_03_2025.students;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class FactoryStudents {
+    private static final Random random = new Random(100);
+    private static List<String> names = List.of("Vany", "Marina", "Katy", "Maks", "Sasha", "Dima");
+
+
+    public static Student next() {
+        String names = next().getName();
+
+        return new Student(names, next().getAge(), next().getGrade());
+    }
+}
+
---
+++ src/L03_03_2025/students/Main.java
@@ -0,0 +1,16 @@
+package L03_03_2025.students;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>(100);
+
+
+
+        boolean isDimaContains = students.contains("Dima");
+        System.out.println(isDimaContains);
+
+    }
+}
