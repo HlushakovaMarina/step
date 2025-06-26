60c49c5ee1b704d72418ac462eea4914cc75c915
HlushakovaMarina <truhina_m@mail.ru>
2025-06-26 21:21:42 +0300

    изучение DOM + задачи

---
+++ src/L26_06_2025/Homework/card_script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",
+ () => {console.log("DOM полностью загружен")
+
+const mainHeader = document.getElementById('mainHeader');
+const age = document.getElementById('age');
+const email = document.getElementById('mail');
+const address = document.getElementById('address');
+const hobby = document.getElementById('hobby');
+const mainText = document.getElementById('mainText');
+const bnt = document.getElementById('btn');
+
+bnt.addEventListener('click', () => {
+    
+})
+
+})
